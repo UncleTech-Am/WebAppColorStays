0f27cd21ea4efd45e2b251b33c3bad164b6404f2
MonaThakur94 <129577513+MonaThakur94@users.noreply.github.com>
2023-10-11 13:05:44 +0530

    commit

---
+++ WebAppColorStays/Models/ViewModel/CsIcon.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppColorStays.Models.ViewModel
+{
+    public class CsIcon
+    {
+        [Key]
+        [StringLength(450)]
+        public string? Id { get; set; }
+        [StringLength(450)]
+        public string Name { get; set; }
+        public string? HtmlCode { get; set; }
+        public bool FreezeStatus { get; set; }
+        public string? FreezedBy { get; set; }
+        [StringLength(450)]
+        public string? Remarks { get; set; }
+        public bool? GlobalStatus { get; set; }
+        public bool? SelectStatus { get; set; }
+        public bool? VerifiedStatus { get; set; }
+        public DateTime? VerifiedOn { get; set; }
+        [StringLength(450)]
+        public string? VerifiedBy { get; set; }
+        public bool? ActiveStatus { get; set; }
+        public DateTime? ActivatedOn { get; set; }
+        [StringLength(450)]
+        public string? ActivatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        [StringLength(450)]
+        public string? CreatedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        [StringLength(450)]
+        public string? ModifiedBy { get; set; }
+        public int? ModificationFrequency { get; set; }
+        [StringLength(450)]
+        public string? GCompId { get; set; }
+
+        [StringLength(450)]
+        public string? CompId { get; set; }
+        [Timestamp]
+        [ConcurrencyCheck]
+        public byte[]? RowVersion { get; set; }
+    }
+}
