9f617251d3ff4247e196465cc7d08905da821c61
Celebrate_Mona <Celebrate_Mona@DESKTOP-1KKIA52>
2025-01-30 17:28:07 +0530

    commit

---
+++ WebAppColorStays/Models/ViewModel/CsKdCnCategory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppColorStays.Models.ViewModel
+{
+    public class CsKdCnCategory
+    {
+        [Key]
+        [StringLength(450)]
+        public string? Id { get; set; }
+        public string? Label { get; set; }
+        [StringLength(450)]
+        [Required(ErrorMessage = "Please enter Name.")]
+        [Remote("CheckDuplicationKdCnCategory", "KdCnCategory", AdditionalFields = ("NameAction, Id"))]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int? Rank { get; set; }
+        public bool FreezeStatus { get; set; }
+        public string? FreezedBy { get; set; }
+        [StringLength(450)]
+        public string? Remarks { get; set; }
+        public bool? GlobalStatus { get; set; }
+        public bool? SelectStatus { get; set; }
+        public bool? VerifiedStatus { get; set; }
+        public DateTime? VerifiedOn { get; set; }
+        [StringLength(450)]
+        public string? VerifiedBy { get; set; }
+        public bool? ActiveStatus { get; set; }
+        public DateTime? ActivatedOn { get; set; }
+        [StringLength(450)]
+        public string? ActivatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        [StringLength(450)]
+        public string? CreatedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        [StringLength(450)]
+        public string? ModifiedBy { get; set; }
+        public int? ModificationFrequency { get; set; }
+        [StringLength(450)]
+        public string? GCompId { get; set; }
+
+        [StringLength(450)]
+        public string? CompId { get; set; }
+        [Timestamp]
+        [ConcurrencyCheck]
+        public byte[]? RowVersion { get; set; }
+    }
+}
---
+++ WebAppColorStays/Models/ViewModel/CsKdCnForm.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppColorStays.Models.ViewModel
+{
+    public class CsKdCnForm
+    {
+        [Key]
+        [StringLength(450)]
+        public string? Id { get; set; }
+        public string? Label { get; set; }
+        public string? Fk_KdRoot_Name { get; set; }
+        [NotMapped]
+        public string? Root { get; set; }
+        public string? Fk_KdPrefix_Name { get; set; }
+        public string? Fk_KdSuffix_Name { get; set; }
+        public bool? IsVeWdBeforePrefix { get; set; }
+        public bool? IsVeWdBeforeSuffix { get; set; }
+        public bool? IsVeWdBeforeRoot { get; set; }
+        public bool? IsVeWdAfterSuffix { get; set; }
+        public bool? IsURLHavingState { get; set; }
+        public bool? IsURLHavingCountry { get; set; }
+        public bool? IsStateKeyword { get; set; }
+        public bool? IsCountryKeyword { get; set; }
+        public bool FreezeStatus { get; set; }
+        public string? FreezedBy { get; set; }
+        [StringLength(450)]
+        public string? Remarks { get; set; }
+        public bool? GlobalStatus { get; set; }
+        public bool? SelectStatus { get; set; }
+        public bool? VerifiedStatus { get; set; }
+        public DateTime? VerifiedOn { get; set; }
+        [StringLength(450)]
+        public string? VerifiedBy { get; set; }
+        public bool? ActiveStatus { get; set; }
+        public DateTime? ActivatedOn { get; set; }
+        [StringLength(450)]
+        public string? ActivatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        [StringLength(450)]
+        public string? CreatedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        [StringLength(450)]
+        public string? ModifiedBy { get; set; }
+        public int? ModificationFrequency { get; set; }
+        [StringLength(450)]
+        public string? GCompId { get; set; }
+
+        [StringLength(450)]
+        public string? CompId { get; set; }
+        [Timestamp]
+        [ConcurrencyCheck]
+        public byte[]? RowVersion { get; set; }
+    }
+}
