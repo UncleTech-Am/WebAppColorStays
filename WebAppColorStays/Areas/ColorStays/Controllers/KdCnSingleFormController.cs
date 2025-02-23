f3ce1a623af0f0c57ef4e5c52794861001140e3e
Celebrate_Mona <Celebrate_Mona@DESKTOP-1KKIA52>
2025-02-23 10:10:28 +0530

    commit

--- WebAppColorStays/Areas/ColorStays/Controllers/KdCnSingleFormController.cs
+++ WebAppColorStays/Areas/ColorStays/Controllers/KdCnSingleFormController.cs
@@ -31,7 +31,7 @@ namespace WebAppColorStays.Areas.ColorStays.Controllers
         //Show the Title in View
         private void Title()
         {
-            ViewBag.Title = "KdCnSingleForm";
+            ViewBag.Title = "Single Keyword Form";
         }
         //Ends
 
